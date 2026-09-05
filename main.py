dcdc32b3d16eaffffb755f07d3b664fa74f762b1
bhoopendramandoliya786-source <bhoopendramandoliya786@gmail.com>
2026-09-05 11:53:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = (
